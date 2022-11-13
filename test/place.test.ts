59479d37f5e45db8678846dd30577432ec0429a6
tachyon83 <83tachyon@gmail.com>
2022-11-13 17:09:56 +0900

    [feat] place test code

--- test/place.test.ts
+++ test/place.test.ts
@@ -1,10 +1,13 @@
 import { AppDataSource } from '../src/data-source'
 import { setNow } from './testUtil'
-import PlaceController from '../src/controllers/place.controller'
-import { PlaceService } from '../src/services/place.service'
+import * as supertest from 'supertest'
+import App from '../src/app'
+
+var request
 
 beforeAll(async () => {
   await AppDataSource.initialize()
+  request = supertest(new App().app)
 })
 afterAll(async () => {
   await AppDataSource.destroy()
@@ -19,20 +22,48 @@ describe('point', () => {
 
   test('place test1', async () => {
     setNow('2022-04-01')
-    // const pCon=new PlaceController(new PlaceLogicCls())
-    const PlaceLogic = new PlaceService()
 
-    const p1 = await PlaceLogic.create({ location: '37.472, 126.9287' }) // 신림
-    const p2 = await PlaceLogic.create({ location: '37.5302, 126.9902' }) // 용산
-    const p3 = await PlaceLogic.create({ location: '37.49738, 127.02824' }) // 강남
-    const p4 = await PlaceLogic.create({ location: '37.5250, 126.8971' }) // 영등포
+    // 신림
+    const res1 = await request
+      .post('/place')
+      .send({ location: '37.472, 126.9287' })
+    expect(res1.statusCode).toEqual(200)
 
-    const l1 = await PlaceLogic.list({
+    // 용산
+    await request.post('/place').send({ location: '37.5302, 126.9902' })
+
+    // 강남
+    await request.post('/place').send({ location: '37.49738, 127.02824' })
+
+    // 영등포
+    await request.post('/place').send({ location: '37.5250, 126.8971' })
+
+    // 아주 작은 반경
+    const res2 = await request.post('/place/list').send({
       location: '37.49735, 127.02823',
-      radius: 100,
+      radius: 0.1,
       numPoints: 50,
     })
-    // expect(l1).toHaveLength(1)
-    console.log(l1)
+    expect(res2.statusCode).toEqual(200)
+    const l2 = JSON.parse(res2.text)
+    expect(l2.data).toHaveLength(1)
+
+    // 전체 좌표
+    const res3 = await request.post('/place/list').send({
+      location: '37.49735, 127.02823',
+      radius: 50,
+      numPoints: 50,
+    })
+    const l3 = JSON.parse(res3.text)
+    expect(l3.data).toHaveLength(4)
+
+    // 적은 숫자의 포인트 요청
+    const res4 = await request.post('/place/list').send({
+      location: '37.49735, 127.02823',
+      radius: 50,
+      numPoints: 2,
+    })
+    const l4 = JSON.parse(res4.text)
+    expect(l4.data).toHaveLength(2)
   })
 })
--- test/testUtil.ts
+++ test/testUtil.ts
@@ -1,11 +1,77 @@
 import * as moment from 'moment'
+import * as supertest from 'supertest'
 import { CommonUtils } from '../src/utils/CommonUtils'
+import App from '../src/app'
 
 jest.setTimeout(20000)
+
+const request = supertest(new App().app)
 
 export function setNow(momentParsableString: string = '2020-01-01 00:00:00') {
   const momentToReturn = moment(momentParsableString)
   jest
     .spyOn(CommonUtils, 'getNowMoment')
     .mockImplementation(() => momentToReturn.clone())
+}
+
+export interface ISupertestObject {
+  [key: string]: string | number
+}
+
+export interface ISupertestSettingArray extends Array<[string, string]> {}
+
+export enum SupertestMethod {
+  POST = 'POST',
+  GET = 'GET',
+  PATCH = 'PATCH',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export interface ISupertestInput {
+  query?: ISupertestObject
+  param?: string
+  body?: ISupertestObject
+  method: SupertestMethod
+  pathIn: string
+  settings?: ISupertestSettingArray
+}
+
+export interface ISupertestOutput {
+  statusCode: number
+  text: string
+}
+
+export function supertestWrapper(input: ISupertestInput): ISupertestOutput {
+  let next = request
+  console.log(49, next, input)
+  const { query, param, body, method, pathIn, settings } = input
+  const path = pathIn + param ?? ''
+
+  switch (method) {
+    case SupertestMethod.POST:
+      next = request.post(path)
+      break
+    case SupertestMethod.GET:
+      next = request.get(path)
+      break
+    default:
+      break
+  }
+  console.log(63, next)
+  if (settings && settings.length > 0) {
+    for (const setting of settings) {
+      next = next.set(setting[0], setting[1])
+    }
+  }
+  console.log(69, next)
+  if (body) {
+    next = next.send(body)
+  }
+  if (query) {
+    next = next.query(query)
+  }
+  console.log(76, next)
+
+  return next.end() as ISupertestOutput
 }
