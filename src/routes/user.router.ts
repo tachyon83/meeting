a367829288e82c78b1f8064a923eed316e93a30c
tachyon <83tachyon@gmail.com>
2022-11-27 15:28:33 +0900

    [feat] user module added along with its stuff

---
+++ src/routes/user.router.ts
@@ -0,0 +1,12 @@
+import * as express from 'express'
+import UserController from '../controllers/user.controller'
+import { jwtAuth } from '../utils/jwt/jwtAuthMiddleware'
+const router = express.Router()
+const userController = new UserController()
+
+router.post('/', userController.create)
+router.post('/login', userController.login)
+// router.get('/', jwtAuth, userController.get)
+// router.patch('/', jwtAuth, userController.update)
+
+export default router
