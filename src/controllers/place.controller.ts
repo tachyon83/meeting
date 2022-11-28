e80ec7f64384ace36ea068240e41eb809c4b0704
tachyon83 <83tachyon@gmail.com>
2022-11-29 02:30:27 +0900

    [feat] expansion and fixes on place module

--- src/controllers/place.controller.ts
+++ src/controllers/place.controller.ts
@@ -1,5 +1,4 @@
 import * as express from 'express'
-import requestHandler from './request.handler'
 import { PlaceService } from '../services/place.service'
 
 export default class PlaceController {
@@ -9,12 +8,12 @@ export default class PlaceController {
     this.service = new PlaceService()
   }
 
-  create = async (req: express.Request, res) => {
-    const data = await this.service.create(req.body)
+  create = async (req: express.Request, res: express.Response) => {
+    const data = await this.service.create(req.body, req.user.userId)
     return res.status(200).json({ data })
   }
 
-  list = async (req: express.Request, res) => {
+  list = async (req: express.Request, res: express.Response) => {
     const data = await this.service.list(req.body)
     return res.status(200).json({ data })
   }
